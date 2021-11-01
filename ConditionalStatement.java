88d48448a2ba499e05d2d5d2e889ffa8e7eb7623
Aleksandr Malyshev <a.malyshev@ymail.com>
2021-11-01 22:47:23 +0300

    check

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -1,41 +1,35 @@
 
 public class ConditionalStatement {	
     public static void main(String[] args) {
-int age = 30;
-boolean menSex=true;
-double height=1.9;
-char firstLetterName='М' ;
+        int age = 30;
+        boolean menSex = true;
+        double height = 1.9;
+        char firstLetterName = 'М';
 
-if(age > 20) {
-    System.out.println("Человеку-"+ age+ " лет"); // Привет
-}
+        if (age > 20) {
+            System.out.println("Человеку -" + age + " лет"); 
 
-if(menSex) {
-    System.out.println("Мужской пол");
-}
+        } if (menSex) {
+            System.out.println("Мужской пол");
 
-if(!menSex) {
-    System.out.println("Женский пол");
-}
+        } if (!menSex) {
+            System.out.println("Женский пол");
 
-if(height < 1.80) {
-	System.out.println("Рост женщины ниже" + height);
+        } if (height < 1.80) {
+	        System.out.println("Рост женщины ниже" + height);
     
-} 
-else { 
-	System.out.println("Мужчина ростом - " + height+ " м.");    
-}
+        } else { 
+            System.out.println("Мужчина ростом - " + height + " м."); 
 
-if(firstLetterName == 'M') {
-    System.out.println("Если первая буква в имени- " +firstLetterName+" -  мужчину зовут Michail");
-} 
+        } if (firstLetterName == 'M') {
+            System.out.println("Если первая буква в имени - " + firstLetterName + " -  мужчину зовут Michail");
 
-else  if(firstLetterName == 'I') {
-    System.out.println("Если первая буква в имени- " +firstLetterName + " - женщину зовут Irina");
+        } else if (firstLetterName == 'I') {
+            System.out.println("Если первая буква в имени - " + firstLetterName + " - женщину зовут Irina");
 
-} else {
-   System.out.println("Человек- не Michail и не Irina"); 
+        } else {
+            System.out.println("Имя - не Michail и не Irina"); 
 
-} 
-       }
+        } 
+    }
 }
--- Cycle.java
+++ Cycle.java
@@ -1,24 +1,22 @@
 public class Cycle {	
     public static void main(String[] args) {
     	
-    for (int i=0; i<=20; i++) {
-    	System.out.println("Цикл for :"+ i);
+    for (int i = 0; i <= 20; i++) {
+    	System.out.println("Цикл for :" + i);
     }
 
-    int value=-6;
-    while (value <=6) {
-         System.out.println("Возвращаем значение Value = "+ value);    
-         value=value+2; 
-}
-    int a=11;
-    int summ=0;
+    int value = - 6;
+    while (value <= 6) {
+    	value = value + 2; 
+        System.out.println("Возвращаем значение Value = " + value);            
+    }
+    int a = 11;
+    int summ = 0;
 
     do {
-    	summ=summ+a;
-    	 a=a+2;
-    	}
-    while (a<20);
-    System.out.println( "Сума нечетных чисел = " + summ);
-    
-        }
+    	summ = summ + a;
+    	a= a + 2;
+    } while (a < 20);
+        System.out.println( "Сума нечетных чисел = " + summ);
+    }
 }
