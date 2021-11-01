aeee4736643f03be322f483f68a29bde4d1fa24a
Aleksandr Malyshev <a.malyshev@ymail.com>
2021-11-01 23:16:38 +0300

    check

--- Cycle.java
+++ Cycle.java
@@ -1,22 +1,19 @@
 public class Cycle {	
-    public static void main(String[] args) {
-    	
-    for (int i = 0; i <= 20; i++) {
-    	System.out.println("Цикл for :" + i);
+    public static void main(String[] args) {	
+        for (int i = 0; i <= 20; i++) {
+    	    System.out.println("Цикл for :" + i);
     }
-
-    int value = - 6;
-    while (value <= 6) {
-    	value = value + 2; 
-        System.out.println("Возвращаем значение Value = " + value);            
+        int value = - 6;
+        while (value <= 6) {
+    	    value = value + 2; 
+            System.out.println("Возвращаем значение Value = " + value);            
     }
-    int a = 11;
-    int summ = 0;
-
-    do {
-    	summ = summ + a;
-    	a= a + 2;
-    } while (a < 20);
-        System.out.println( "Сума нечетных чисел = " + summ);
+        int a = 11;
+        int summ = 0;
+        do {
+    	    summ = summ + a;
+    	    a= a + 2;
+            System.out.println("Сума нечетных чисел = " + summ);
+         } while (a < 20);          
     }
 }
