17b58e2d44fa809142e654036ba5c01764d5cfb0
Aleksandr Malyshev <a.malyshev@ymail.com>
2021-11-02 17:01:33 +0300

    New

---
+++ Lesson1/Varables.java
@@ -0,0 +1,23 @@
+
+
+public class Varables {
+    public static void main(String[] args) {
+    	byte core = 4;
+    	short ssd = 500;
+    	int countSsd = 2;
+    	long id = 77604321;
+    	float freqProc = 3.4f;
+        double usb = 3.0;
+        char weight = 'g';
+        boolean windows = true;
+
+        System.out.println("Количество ядер : " + core);
+        System.out.println("Объем жесткого диска : " + ssd + " ГБ");
+        System.out.println("Число жестких дисков : " + countSsd);
+        System.out.println("Серийный номер : " + id);
+        System.out.println("Частота процессора : " + freqProc + " ГГц");
+        System.out.println("Тип  USB : " + usb);
+        System.out.println("Масса компьютера 5000 " + weight );
+        System.out.println("Операционная система Windows? " + windows);
+    }
+}
