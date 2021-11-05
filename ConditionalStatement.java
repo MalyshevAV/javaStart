296bc3d0cc325cfa7d05ab566295a444b72e2d6d
Malyshev Alexandr <90769360+MalyshevAV@users.noreply.github.com>
2021-11-05 16:36:17 +0300

    Add files via upload

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -1,35 +1,29 @@
-
 public class ConditionalStatement {	
     public static void main(String[] args) {
         int age = 30;
-        boolean menSex = true;
-        double height = 1.9;
-        char firstLetterName = 'М';
-
         if (age > 20) {
             System.out.println("Человеку -" + age + " лет"); 
-
-        } if (menSex) {
+        } 
+        boolean menSex = true;
+        if (menSex) {
             System.out.println("Мужской пол");
-
-        } if (!menSex) {
+        } 
+        if (!menSex) {
             System.out.println("Женский пол");
-
-        } if (height < 1.80) {
+        } 
+        double height = 1.9;
+        if (height < 1.80) {
 	        System.out.println("Рост женщины ниже" + height);
-    
         } else { 
             System.out.println("Мужчина ростом - " + height + " м."); 
-
-        } if (firstLetterName == 'M') {
+        } 
+        char firstLetterName = 'М';
+        if (firstLetterName == 'M') {
             System.out.println("Если первая буква в имени - " + firstLetterName + " -  мужчину зовут Michail");
-
         } else if (firstLetterName == 'I') {
             System.out.println("Если первая буква в имени - " + firstLetterName + " - женщину зовут Irina");
-
         } else {
             System.out.println("Имя - не Michail и не Irina"); 
-
         } 
     }
 }
