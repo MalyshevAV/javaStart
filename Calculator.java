a88e5fddb15e7a75d770b1f07157fe9c17013ab4
Aleksandr Malyshev <a.malyshev@ymail.com>
2021-11-05 14:53:53 +0300

    добавил faila

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+ public class Calculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        char operator = '/';
+        int value = a * a;
+
+        if (a > 0 && b > 0 && operator == '+') {
+            System.out.println("a + b = " + (a + b));
+        } else if (a > 0 && b > 0 && operator == '/') {
+            System.out.println("a / b = " + (a / b));
+        } else if (a > 0 && b > 0 && operator == '*') {
+            System.out.println("a * b = " + (a * b));
+        } else if (a > 0 && b > 0 && operator == '/') {
+            System.out.println("a / b = " + (a / b));
+        } else if (a > 0 && b > 0 && operator == '%') {
+            System.out.println("a % b = " + (a % b));
+        } else if (a > 0 && b > 0 && value != 0) {
+            System.out.println("a^2 = " + value);
+        } else {
+            System.out.println("Введены неверные значания");
+        }
+    }
+}
---
+++ Cycle.java
@@ -0,0 +1,20 @@
+public class Cycle {	
+    public static void main(String[] args) {	
+        for (int i = 0; i <= 20; i++) {
+    	    System.out.println("Цикл for :" + i);
+    }
+        int value = - 6;
+        while (value <= 6) {
+    	    value = value + 2; 
+            System.out.println("Возвращаем значение Value = " + value);            
+    }
+        int a = 11;
+        int summ = 0;
+        do {
+    	    summ = summ + a;
+    	    a= a + 2;
+            System.out.println("Сума нечетных чисел = " + summ);
+        } 
+        while (a < 20);          
+    }
+}
