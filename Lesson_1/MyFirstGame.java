616abdf32258897d8075c4bc681a663e207a81f1
Aleksandr Malyshev <a.malyshev@ymail.com>
2021-11-07 22:23:04 +0300

    Правка

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,16 +1,15 @@
 public class MyFirstGame {	
     public static void main(String[] args) {
-    int setNumber = 90;
+        int hiddenNumber = 90;
         
-    for (int i = 1; i <= 100; i++) {
-        if (i > setNumber) {
-            System.out.println("Данное число больше того, что загадал компьютер");
-        } else if (i < setNumber) {
-            System.out.println("Данное число меньше того, что загадал компьютер");
-        } else {
-            System.out.println("Поздравляем! Вы угадали число " + setNumber);
-            break; 
-        }
+        for (int i = 1; i <= 100; i++) {
+            if (i > hiddenNumber) {
+                System.out.println("Данное число больше того, что загадал компьютер");
+            } else if (i < hiddenNumber) {
+                System.out.println("Данное число меньше того, что загадал компьютер");
+            } else {
+                System.out.println("Поздравляем! Вы угадали число " + hiddenNumber);
+            }
         }  
     }           
 }
