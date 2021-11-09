e9905f8c4df36469cca326c41d363160f921d87c
Aleksandr Malyshev <a.malyshev@ymail.com>
2021-11-09 20:41:32 +0300

    добавил файл

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,7 +2,7 @@ public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
         wolf.age = 5;
-        wolf.color= "grey";
+        wolf.color = "grey";
         wolf.name = "Jack";
         wolf.sex = "male";
         wolf.weight = 20;
