2d5b16bf2d734ac0b124e17e8b1c51c99fdcddad
Aleksandr Malyshev <a.malyshev@ymail.com>
2021-11-11 23:27:31 +0300

    chek

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,27 +1,27 @@
 public class Calculator {
 
-    public static int calc(int numb1, int numb2, char operator) {
+    public static int calc(int num1, char operator, int num2) {
         int result = 0;
         switch (operator) {
             case '+':
-                result = numb1 + numb2;
+                result = num1 + num2;
                 break;
             case '/':
-                result = numb1 / numb2;
+                result = num1 / num2;
                 break;
             case '*':
-                result = numb1 * numb2;
+                result = num1 * num2;
                 break;
             case '-':
-                result = numb1 - numb2;
+                result = num1 - num2;
                 break;
             case '%':
-                result = numb1 % numb2;
+                result = num1 % num2;
                 break;
             case '^':
-                result = numb1;
-                for (int i = 1; i < numb2; i++) {
-                    result *= numb1;
+                result = num1;
+                for (int i = 1; i < num2; i++) {
+                    result *= num1;
                 }
                 break;
             default:
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -5,16 +5,19 @@ public class CalculatorTest {
 	    while (true) {
 	        Scanner scanner = new Scanner(System.in);
 	        System.out.println("Введите первое число:");
-	        int numb1 = scanner.nextInt();
+	        int num1 = scanner.nextInt();
 	        System.out.println("Введите знак математической операции:");
 	        char operator = scanner.next().charAt(0);
 	        System.out.println("Введите второе число число:");
-	        int numb2 = scanner.nextInt();
+	        int num2 = scanner.nextInt();
 	        System.out.println("Результат: ");
-	        int value = Calculator.calc(numb1, numb2, operator);
-	        System.out.println(value);
-	        System.out.println("Хотите продолжить вычисления? [yes/no]:");
-	        String answer = scanner.next();
+	        int result = Calculator.calc(num1, operator, num2);
+	        System.out.println(result);
+            String answer="";
+            while (!answer.equals("no") && !answer.equals("yes")) {
+                System.out.println("Хотите продолжить вычисления? [yes/no]:");
+	            answer = scanner.next();
+            }
 	        if (answer.equals("no")) {
 	            break;
 	        }
