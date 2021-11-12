3e91a45774f51d1ebddb7aa02445ac851cf788a6
Aleksandr Malyshev <a.malyshev@ymail.com>
2021-11-12 19:47:33 +0300

    Правки

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,25 +2,26 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-	    while (true) {
-	        Scanner scanner = new Scanner(System.in);
-	        System.out.println("Введите первое число:");
-	        int num1 = scanner.nextInt();
-	        System.out.println("Введите знак математической операции:");
-	        char operator = scanner.next().charAt(0);
-	        System.out.println("Введите второе число число:");
-	        int num2 = scanner.nextInt();
-	        System.out.println("Результат: ");
-	        int result = Calculator.calc(num1, operator, num2);
-	        System.out.println(result);
-            String answer="";
+        Scanner scanner = new Scanner(System.in);
+        String answer = "";
+        while (answer.equals("no")) {
+            break;
+        }
+        while (!answer.equals("no")) {
+            System.out.println("Введите первое число:");
+            int num1 = scanner.nextInt();
+            System.out.println("Введите знак математической операции:");
+            char operator = scanner.next().charAt(0);
+            System.out.println("Введите второе число число:");
+            int num2 = scanner.nextInt();
+            System.out.println("Результат: ");
+            int result = Calculator.calc(num1, operator, num2);
+            System.out.println(result);
+
             while (!answer.equals("no") && !answer.equals("yes")) {
                 System.out.println("Хотите продолжить вычисления? [yes/no]:");
-	            answer = scanner.next();
+                answer = scanner.next();
             }
-	        if (answer.equals("no")) {
-	            break;
-	        }
-	    }
-	}
+        }
+    }
 }
